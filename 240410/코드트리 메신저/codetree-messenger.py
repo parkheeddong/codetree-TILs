5f44f50b149085e468a6f6c06b0f2163d1c067e8
parkheeddong <116873401+parkheeddong@users.noreply.github.com>
2024-04-10 19:40:09 +0900

    Title: 코드트리 메신저, Time: 8037ms, Memory: 50MB, Status: Time Limit Exceed - CodeTree

--- 240410/코드트리 메신저/codetree-messenger.py
+++ 240410/코드트리 메신저/codetree-messenger.py
@@ -1,5 +1,5 @@
 n, q = map(int, input().split()) # n = 채팅방 수, q 명령 수
-inputlist = list(map(int,  input().split()))
+inputlist = list(map(int, input().split()))
 parent = [0] + inputlist[1:n+1] # 부모 채팅방 번호
 authority = [0] + inputlist[n+1:] # 권한 세기
 alarm = [True]*(n+1) # 알람 온오프
@@ -11,21 +11,35 @@ def init() :
     return children
 
 def find_available_chat(c, children, root) :
-    cnt = 0
-    childs = children[c]
-    if childs == [] :
-        return 0
-    for child in childs:
-        if alarm[child] == False :
-            continue
-        if authority[child] >= root :
-            cnt += 1
-        cnt += find_available_chat(child, children, root + 1)
-    return cnt
+    # c에 대해서 알림에 도달할 수 있는 하위 채팅방 개수 구하기
+    answer = 0
+    nodes = []
+    cnt = 1
+    for i in children[c] :
+        if alarm[i] == True and authority[i] >= cnt :
+            nodes.append(i)
+            answer += 1
+    # nodes = 가능한 채팅방을 넣는 리스트 [3, 4]
+
+    while nodes :
+        cnt += 1
+        new_nodes = []
+        for i in nodes :
+            for j in children[i] :
+                if alarm[j] == False :
+                    continue 
+                if authority[j] >= cnt :
+                    answer += 1
+                    new_nodes.append(j)
+                else :
+                    new_nodes.append(j)
+        nodes = new_nodes
+
+    return answer
 
 children = init()
 for _ in range(q-1) :
-    order = list(map(int,  input().split()))
+    order = list(map(int, input().split()))
     if order[0] == 200 :
         c = order[1]
         if alarm[c] :
